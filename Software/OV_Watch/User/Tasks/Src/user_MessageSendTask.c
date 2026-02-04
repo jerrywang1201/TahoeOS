8a30285128235af26aa2bb161993c2fb2c76be34
jerrywang1201 <jerrywang183@yahoo.com>
2026-02-04 16:17:54 -0500

    fix(rtc-timeformat): return status from TimeFormat_Get to avoid UB\n\nTested: not run (no CLI build available for OV_Watch)

--- Software/OV_Watch/User/Tasks/Src/user_MessageSendTask.c
+++ Software/OV_Watch/User/Tasks/Src/user_MessageSendTask.c
@@ -73,8 +73,10 @@ uint8_t TimeFormat_Get(uint8_t * str)
 		RTC_SetDate(TimeSetMessage.nowdate.Year, TimeSetMessage.nowdate.Month,TimeSetMessage.nowdate.Date);
 		RTC_SetTime(TimeSetMessage.nowtime.Hours,TimeSetMessage.nowtime.Minutes,TimeSetMessage.nowtime.Seconds);
 		printf("TIMESETOK\r\n");
+		return 1;
 	}
 
+	return 0;
 }
 
 /**
@@ -134,5 +136,4 @@ void MessageSendTask(void *argument)
 		osDelay(1000);
 	}
 }
-
 
